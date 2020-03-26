d98dc2746e913b6547e8786e150b917d5c0f0486
ofshellohicy <ofshellohicy@gmail.com>
2020-03-26 12:13:46 +0800

    camera test

--- test/camera_test.py
+++ test/camera_test.py
@@ -1,14 +1,13 @@
-#coding:utf-8
+# coding:utf-8
+import numpy as np
 import sys
 sys.path.append('..')
-from Detection.MtcnnDetector import MtcnnDetector
-from Detection.detector import Detector
-from Detection.fcn_detector import FcnDetector
-from train_models.mtcnn_model import P_Net, R_Net, O_Net
 import cv2
-import numpy as np
+from train_models.mtcnn_model import P_Net, R_Net, O_Net
+from Detection.fcn_detector import FcnDetector
+from Detection.detector import Detector
+from Detection.MtcnnDetector import MtcnnDetector
 
-test_mode = "onet"
 thresh = [0.9, 0.6, 0.7]
 min_face_size = 24
 stride = 2
@@ -16,8 +15,14 @@ slide_window = False
 shuffle = False
 #vis = True
 detectors = [None, None, None]
-prefix = ['../data/MTCNN_model/PNet_landmark/PNet', '../data/MTCNN_model/RNet_landmark/RNet', '../data/MTCNN_model/ONet_landmark/ONet']
+prefix = [
+    '../data/MTCNN_model/PNet_landmark/PNet',
+    '../data/MTCNN_model/RNet_landmark/RNet',
+    '../data/MTCNN_model/ONet_landmark/ONet'
+]
+
 epoch = [18, 14, 16]
+
 model_path = ['%s-%s' % (x, y) for x, y in zip(prefix, epoch)]
 PNet = FcnDetector(P_Net, model_path[0])
 detectors[0] = PNet
@@ -25,21 +30,33 @@ RNet = Detector(R_Net, 24, 1, model_path[1])
 detectors[1] = RNet
 ONet = Detector(O_Net, 48, 1, model_path[2])
 detectors[2] = ONet
-videopath = "./video_test.avi"
-mtcnn_detector = MtcnnDetector(detectors=detectors, min_face_size=min_face_size,
-                               stride=stride, threshold=thresh, slide_window=slide_window)
 
-video_capture = cv2.VideoCapture(videopath)
-video_capture.set(3, 340)
-video_capture.set(4, 480)
+videopath = "./video_test.avi"
+mtcnn_detector = MtcnnDetector(detectors=detectors,
+                               min_face_size=min_face_size,
+                               stride=stride,
+                               threshold=thresh,
+                               slide_window=slide_window)
+
+CAMERA_ANY = 0
+video_capture = cv2.VideoCapture(CAMERA_ANY)
+# read from video
+# video_capture = cv2.VideoCapture(videopath)
+
+WIDTH = 320
+HEIGHT = 240
+video_capture.set(3, WIDTH)
+video_capture.set(4, HEIGHT)
+
 corpbbox = None
+
 while True:
     # fps = video_capture.get(cv2.CAP_PROP_FPS)
     t1 = cv2.getTickCount()
     ret, frame = video_capture.read()
     if ret:
         image = np.array(frame)
-        boxes_c,landmarks = mtcnn_detector.detect(image)
+        boxes_c, landmarks = mtcnn_detector.detect(image)
 
         print(landmarks.shape)
         t2 = cv2.getTickCount()
@@ -52,13 +69,16 @@ while True:
             # if score > thresh:
             cv2.rectangle(frame, (corpbbox[0], corpbbox[1]),
                           (corpbbox[2], corpbbox[3]), (255, 0, 0), 1)
-            cv2.putText(frame, '{:.3f}'.format(score), (corpbbox[0], corpbbox[1] - 2), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
-                        (0, 0, 255), 2)
-        cv2.putText(frame, '{:.4f}'.format(t) + " " + '{:.3f}'.format(fps), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
-                    (255, 0, 255), 2)
+            cv2.putText(frame, '{:.3f}'.format(score),
+                        (corpbbox[0], corpbbox[1] - 2),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+        cv2.putText(frame, '{:.4f}'.format(t) + " " + '{:.3f}'.format(fps),
+                    (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 255), 2)
         for i in range(landmarks.shape[0]):
-            for j in range(len(landmarks[i])/2):
-                cv2.circle(frame, (int(landmarks[i][2*j]),int(int(landmarks[i][2*j+1]))), 2, (0,0,255))            
+            for j in range(len(landmarks[i]) / 2):
+                cv2.circle(frame, (int(
+                    landmarks[i][2 * j]), int(int(landmarks[i][2 * j + 1]))),
+                           2, (0, 0, 255))
         # time end
         cv2.imshow("", frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -66,5 +86,6 @@ while True:
     else:
         print('device not find')
         break
+
 video_capture.release()
 cv2.destroyAllWindows()
